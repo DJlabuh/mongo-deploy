e388d711b5a19ecbb6c1286647704fe97a1bcfa2
DJlabuh <djlabuh@gmail.com>
2024-11-18 19:15:45 +0200

    Initialize project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, (err) => {
+  err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
