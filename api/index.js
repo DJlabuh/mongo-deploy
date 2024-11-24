210fde2d35e944ae7834ab797290557158f90ed7
DJlabuh <djlabuh@gmail.com>
2024-11-24 14:21:57 +0200

    Remove unnecessary local environment code

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const dotenv = require("dotenv");
 const baseRoutes = require("../routes/base-routes"); // Підключення базових маршрутів
 const movieRoutes = require("../routes/movie-routes"); // Підключення маршрутів для фільмів
 
-dotenv.config(); // Завантажуємо змінні середовища
+  dotenv.config(); // Завантажуємо змінні середовища
 
 const app = express();
-const PORT = process.env.PORT || 3000;  // Від Vercel буде надано port через process.env.PORT
+
 const URL = process.env.MONGO_URI; // Підключення до MongoDB з .env файлу
 
 // Middleware для обробки JSON
@@ -19,14 +19,11 @@ app.use("/", baseRoutes); // Базовий маршрут на кореневу
 // Використання маршрутів для фільмів
 app.use("/movies", movieRoutes); // Усі маршрути для фільмів починаються з '/movies'
 
+// Підключення до MongoDB
 mongoose
   .connect(URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to database:", err.message));
 
-app.listen(PORT, () => {
-  // Виводити лог тільки локально, на Vercel сервер автоматично запускається
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  }
-});
+// Експортуємо додаток для використання на Vercel (Vercel самостійно запускає додаток)
+module.exports = app;
