66bb5b31b19ea85e1b6a3fe543f3bc60cd704606
DJlabuh <djlabuh@gmail.com>
2024-11-24 13:52:03 +0200

    Updated server setup for Vercel

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -5,14 +5,14 @@ const {
   deleteMovie,
   addMovie,
   updateMovie,
-} = require("../controllers/movie-controllers");
+} = require("../controllers/movie-controllers"); // Змінено шлях на "../controllers/movie-controllers"
 
 const router = express.Router();
 
-router.get("/", getMovies); // Маршрут для отримання списку фільмів
-router.get("/:id", getMovie); // Маршрут для отримання одного фільму за id
-router.delete("/:id", deleteMovie); // Маршрут для видалення фільму за його id
-router.post("/", addMovie); // Маршрут для додавання фільму
-router.patch("/:id", updateMovie); // Маршрут оновлення фільму за ID
+router.get("/", getMovies); // /movies для отримання списку фільмів
+router.get("/:id", getMovie); // /movies/:id для отримання одного фільму за id
+router.delete("/:id", deleteMovie); // /movies/:id для видалення фільму
+router.post("/", addMovie); // /movies для додавання фільму
+router.patch("/:id", updateMovie); // /movies/:id для оновлення фільму
 
 module.exports = router;
